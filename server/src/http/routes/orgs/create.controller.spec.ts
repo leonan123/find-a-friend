800c72e84c0f0b843703ab1ffd1ffd2b8631d417
leonan <leonanorth3@gmail.com>
2025-05-23 01:20:37 -0300

    test(e2e): create organization route

--- server/src/http/routes/orgs/create.controller.spec.ts
+++ server/src/http/routes/orgs/create.controller.spec.ts
@@ -1,7 +1,30 @@
-import { describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { app } from '@/app'
 
 describe('Create organization controller', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
   it('should be able to create an organization', async () => {
-    expect(1 + 1).toBe(2)
+    const response = await request(app.server).post('/orgs').send({
+      responsibleName: 'John Doe',
+      email: 'V4V5T@example.com',
+      zipCode: '12345678',
+      neighborhood: 'Neighborhood 1',
+      IBGECityCode: '1234567',
+      stateCode: 'RS',
+      address: 'Address 1',
+      whatsapp: '123456789',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
   })
 })
